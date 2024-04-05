a717c6128162a4e3139f48d55504b907bee3cd2e
Hiago Silva <hiagosilvaanjos7@gmail.com>
2024-04-04 22:47:22 -0300

    preparado o ambiente de desenvolvimento | interface inicial feita

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,8 +2,17 @@ import nlwIcon from "../assets/nlw-unit-icon.svg";
 
 export function Header() {
   return (
-    <div>
+    <div className="flex items-center gap-5 py-2">
       <img src={nlwIcon} />
+
+      <nav className="flex items-center gap-5">
+        <a className="font-medium text-sm text-zinc-300" href="">
+          Eventos
+        </a>
+        <a className="font-medium text-sm" href="">
+          Participantes
+        </a>
+      </nav>
     </div>
   );
 }
